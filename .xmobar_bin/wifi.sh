f1c7f917beaf8958cb9dd278f9f3a46f8e232dc6
Rane Brown <rane.brown@gmail.com>
2016-01-12 21:22:25 -0700

    Modified example to create working wifi monitor

--- .xmobar_bin/wifi.sh
+++ .xmobar_bin/wifi.sh
@@ -3,30 +3,31 @@
 # Signal: is reported in a wireless power ratio in dbm (e.g. from -100 to 0). The closer the negative value gets to zero, the better the signal. Observing the reported power on a good quality link and a bad one should give an idea about the individual range. 
 
 # check for ethernet connection and exit if true
-iwconfig wlan0 2>&1 | grep -q no\ wireless\ extensions\. && {
-  echo wired
-  exit 0
-}
+#iwconfig wlan0 2>&1 | grep -q no\ wireless\ extensions\. && {
+#  echo wired
+#  exit 0
+#}
 
-ssid=`iw dev wlp2s0 link | awk -F '"' '/ESSID/ {print $2}'`
-stngth=`iw dev wlp2s0 link | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
-bars=`expr $stngth / 10`
+ssid=`iw dev wlp2s0 link | awk -F SSID: '{print $2}'`
+strength=`iw dev wlp2s0 link | awk -F signal: '{print $2}' | cut -c3-5`
+
+bars=`expr $strength / 10`
 
 case $bars in
-  0)  bar='[----------]' ;;
-  1)  bar='[/---------]' ;;
-  2)  bar='[//--------]' ;;
-  3)  bar='[///-------]' ;;
-  4)  bar='[////------]' ;;
+  10) bar='[----------]' ;;
+  9)  bar='[/---------]' ;;
+  8)  bar='[//--------]' ;;
+  7)  bar='[///-------]' ;;
+  6)  bar='[////------]' ;;
   5)  bar='[/////-----]' ;;
-  6)  bar='[//////----]' ;;
-  7)  bar='[///////---]' ;;
-  8)  bar='[////////--]' ;;
-  9)  bar='[/////////-]' ;;
-  10) bar='[//////////]' ;;
+  4)  bar='[//////----]' ;;
+  3)  bar='[///////---]' ;;
+  2)  bar='[////////--]' ;;
+  1)  bar='[/////////-]' ;;
+  0)  bar='[//////////]' ;;
   *)  bar='[----!!----]' ;;
 esac
 
-echo $essid $bar
+echo $ssid $bar
 
 exit 0
