3fbb7032ee03baa411f901c9e66cdffd5a5e5a92
rane <rane.brown@gmail.com>
2016-02-10 18:07:17 -0700

    modiifed args for ctags

--- .bin/ctagsDeps.sh
+++ .bin/ctagsDeps.sh
@@ -5,4 +5,5 @@
 # final pipe to awk removes the first line which is a reference to the object file
 # use: ctagsDeps -Ipath/to/header ... file1 file2 ...
 # ctags -L flag reads a list of files if - is used for filename then files are read from stdin
-gcc -M $* | awk '{print $1}' | awk '{if(NR>1)print}' | ctags --append=yes --c-kinds=+p --fields=+liaS --extra=+q -f.tags -L -
+gcc -M $* | awk '{print $1}' | awk '{if(NR>1)print}' | ctags --append=yes --c-kinds=+p -f.tags -L -
+# --fields=+liaS --extra=+q 
